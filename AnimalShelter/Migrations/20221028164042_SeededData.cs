6713bd8c0478ad95e8e7adbf14bef5e735dbbb85
tgold83 <ted.jonathan.gold@gmail.com>
2022-10-28 16:24:27 -0700

    add migrations to project

---
+++ AnimalShelter/Migrations/20221028164042_SeededData.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class SeededData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Name", "Species" },
+                values: new object[,]
+                {
+                    { 1, 7, "Scully", "Dog" },
+                    { 2, 6, "Blaze", "Dog" },
+                    { 3, 13, "Kitty", "Cat" },
+                    { 4, 1, "Newt", "Dog" },
+                    { 5, 4, "Ripley", "Dog" },
+                    { 6, 14, "Kevin", "Cat" },
+                    { 7, 12, "Tank", "Dog" },
+                    { 8, 1, "Yuzu", "Dog" },
+                    { 9, 2, "Tzufie", "Dog" },
+                    { 10, 9, "Lou", "Dog" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 10);
+        }
+    }
+}
